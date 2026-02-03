723ff0eb6549e29b2e2c1f6469c1af810b7bec18
Gabriel <gabriel.andradearce@gmail.com>
2026-02-03 19:38:08 -0300

    mensagem de alteração

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
 	public static void main (String[] args) {
 		System.out.println("ta funcionando?");
+		System.out.println("gostei desse exercicio, vou superar vc logo¹");
 	}
 }
